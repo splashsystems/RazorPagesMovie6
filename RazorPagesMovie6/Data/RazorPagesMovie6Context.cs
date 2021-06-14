a7b172d236c946fae3bdac4fb8d3fcee5ed0d632
Scot <bscot.hagen@gmail.com>
2021-06-14 10:37:43 -0700

    Scaffolded

---
+++ RazorPagesMovie6/Data/RazorPagesMovie6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie6.Models;
+
+namespace RazorPagesMovie6.Data
+{
+    public class RazorPagesMovie6Context : DbContext
+    {
+        public RazorPagesMovie6Context (DbContextOptions<RazorPagesMovie6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie6.Models.Movie> Movie { get; set; }
+    }
+}
--- RazorPagesMovie6/Startup.cs
+++ RazorPagesMovie6/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie6.Data;
 
 namespace RazorPagesMovie6
 {
@@ -24,6 +26,9 @@ namespace RazorPagesMovie6
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<RazorPagesMovie6Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("RazorPagesMovie6Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
