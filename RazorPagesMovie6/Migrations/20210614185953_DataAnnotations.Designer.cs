3347441f94737b88e26f21cd2819dd6dc98be50f
Scot <bscot.hagen@gmail.com>
2021-06-14 12:00:47 -0700

    Added Rating field and Validations

--- RazorPagesMovie6/Migrations/20210614185953_DataAnnotations.Designer.cs
+++ RazorPagesMovie6/Migrations/20210614185953_DataAnnotations.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovie6.Data;
 
 namespace RazorPagesMovie6.Migrations
 {
     [DbContext(typeof(RazorPagesMovie6Context))]
-    partial class RazorPagesMovie6ContextModelSnapshot : ModelSnapshot
+    [Migration("20210614185953_DataAnnotations")]
+    partial class DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
